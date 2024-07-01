09c46eacc2a865fe6873cc0f22ad8274567dfdc6
rashitiwari18 <166488281+rashitiwari18@users.noreply.github.com>
2024-07-01 18:34:07 +0530

    update operators

---
+++ 3.operators.py
@@ -0,0 +1,12 @@
+# ---ARITHMETIC OPERATOR----
+
+a= 25
+b=10
+
+print("the value of", a , "+", b ,"is", a+b)
+print("the value of", a , "-", b ,"is",a-b)
+print("the value of", a , "*", b ,"is",a*b)
+print("the value of", a , "**", b ,"is",a**b)
+print("the value of", a , "/", b ,"is",a/b)
+print("the value of", a , "//", b ,"is",a//b)
+print("the value of", a , "%", b ,"is",a%b)
