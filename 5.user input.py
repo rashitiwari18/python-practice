c441e9e4e1b84d2e5a4b5ce9a82daecdfd3972b2
rashitiwari18 <166488281+rashitiwari18@users.noreply.github.com>
2024-07-03 16:34:17 +0530

    update user input

---
+++ 5.user input.py
@@ -0,0 +1,11 @@
+# a=input()
+# print("My name is", a)
+
+# name=input("enter your name")
+# print("my name is",name)
+
+# --STRING--
+a = input(" enter first number")
+b = input(" enter second number")
+print( a + b )
+print( int(a) + int(b) )
