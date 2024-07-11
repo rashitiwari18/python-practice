33af8134404a83c46ec594321000ab4a0ec18a45
rashitiwari18 <166488281+rashitiwari18@users.noreply.github.com>
2024-07-11 18:35:02 +0530

    update string slicing

---
+++ 7.string slicing and operations.py
@@ -0,0 +1,15 @@
+fruit= "mango"
+length= len(fruit)
+
+print( "mango is a", length, "letter word")
+
+print( fruit[ 0:4 ])  # including 0 but not 4
+# print( fruit[ :4 ])
+# print( fruit[ 1: ])
+# print( fruit[ : ])
+
+#negative slicing
+print( fruit[ 0: -3])
+
+print( fruit[ -1: -3 ])
+print( fruit[-3 : -1])
