d8512f6100d55d5ea5e82e58da5ad86592ddb7b6
rashitiwari18 <166488281+rashitiwari18@users.noreply.github.com>
2024-07-13 10:05:14 +0530

    update string methods

---
+++ 8.string methods.py
@@ -0,0 +1,8 @@
+# strings are immutable
+a = " !! Rashi !!! "
+print(a)
+print(len(a))     # returns the length of the string
+
+print(a.upper())  # converts lowercase to uppercase
+print(a.lower())  # converts uppercase to lowercase
+print(a.title())  # converts first alphabet of each letter capital
