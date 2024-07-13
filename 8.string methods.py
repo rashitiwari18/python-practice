5b1accfa90770076386fbf3cc9784704f4b95c0f
rashitiwari18 <166488281+rashitiwari18@users.noreply.github.com>
2024-07-13 10:06:32 +0530

    update again

--- 8.string methods.py
+++ 8.string methods.py
@@ -6,3 +6,7 @@ print(len(a))     # returns the length of the string
 print(a.upper())  # converts lowercase to uppercase
 print(a.lower())  # converts uppercase to lowercase
 print(a.title())  # converts first alphabet of each letter capital
+
+print(a.rstrip("!"))                    # removes only end drilling characters
+print(a.replace("Rashi", "Tashu"))      # if there was more occurences all would have changed
+print(a.split(" "))                     # splits wherever it gets " " as mentioned and converts it into list
