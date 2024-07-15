5769b34ef73f77b520c3749a8d969050094959b4
rashitiwari18 <166488281+rashitiwari18@users.noreply.github.com>
2024-07-15 19:38:03 +0530

    update if else statement

---
+++ 9.if else statements.py
@@ -0,0 +1,12 @@
+# If- else statement example:--
+
+price = int( input( " Enter price of apples"))
+budget = 200
+print(" Price of apples are: ", price)
+print(" My budget is: ", budget)
+
+if (budget > price):
+    print(" Alexa, add 1kg apple to the cart ")
+else:
+    print(" Alexa, do not add apples to the cart")
+print(" Keep shopping !!")  
